2da31624e30f45f8956fad0902d4252435ea4870
raissautf <raissap@alunos.utfpr.edu.br>
2023-05-24 19:06:29 -0300

    setup mobile finalizada

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Hello, Raissa</Text>
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-gray-50">Hello, Raissa</Text>
       <StatusBar style="light" translucent />
     </View>
   )
