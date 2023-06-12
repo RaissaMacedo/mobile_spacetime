e9a1ef0c99b70a42f61a42fac3d2ca033a79019e
raissautf <raissap@alunos.utfpr.edu.br>
2023-06-12 18:25:51 -0300

    autenticacao

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,27 +1,71 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
-
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 import { styled } from 'nativewind'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/bd8f5be4a8cc136e279c',
+}
 
 export default function App() {
+  const router = useRouter()
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: 'bd8f5be4a8cc136e279c',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'spacetime ',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+    const { token } = response.data
+    await SecureStore.setItemAsync('token', token)
+    // rota que quer enviar o usuario
+    router.push('/memories')
+  }
+  // toda vez a var response mudar o seu valor, vai executar o cod esta dentrodo useEffect
+  useEffect(() => {
+    /* console.log(
+      makeRedirectUri({
+        scheme: 'spacetime ',
+      }),
+    ) */
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
+
   // carregando as fontes
   if (!hasLoadedFonts) {
     return null
@@ -47,6 +91,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm font-bold uppercase text-black">
             Cadastrar lembrança
